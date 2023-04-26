1d21d55400473b72fcf488ee1130b2f4186ce516
MiracTechPro <mirac.techproed@gmail.com>
2023-04-26 23:27:50 +0300

    DeleteMapping-GetMapping

--- 08- SpringBoot/springbootintro/src/main/java/com/tpe/SpringbootintroApplication.java
+++ 08- SpringBoot/springbootintro/src/main/java/com/tpe/SpringbootintroApplication.java
@@ -11,7 +11,6 @@ public class SpringbootintroApplication {
 		SpringApplication.run(SpringbootintroApplication.class, args);
 
 
-
 	}
 
 }
--- 08- SpringBoot/springbootintro/src/main/java/com/tpe/controller/StudentController.java
+++ 08- SpringBoot/springbootintro/src/main/java/com/tpe/controller/StudentController.java
@@ -3,13 +3,15 @@ package com.tpe.controller;
 import com.tpe.domain.Student;
 import com.tpe.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/students") // http://localhost:8080/students
@@ -19,12 +21,55 @@ public class StudentController {
     private StudentService studentService;
 
     // !!! Butun ogrencileri getirelim
-    @GetMapping // http://localhost:8080/students + GET
+    @GetMapping // http://localhost:8080/students/ + GET
     public ResponseEntity<List<Student>>  getAll(){
         List<Student> students = studentService.getAll();
 
         return ResponseEntity.ok(students); // List<Student> + HTTP-StatusCode(200)
+    }
+
+    // !!! Create new student
+    @PostMapping // http://localhost:8080/students/ + POST + JSON
+    public ResponseEntity<Map<String,String>> createStudent( @Valid @RequestBody Student student){
+        studentService.createStudent(student);
+
+        Map<String,String> map = new HashMap<>();
+        map.put("message", "Student is created successfuly");
+        map.put("status", "true");
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED); // 201
+    }
+
+    //!!! Get a Student by ID via RequestParam
+    @GetMapping("/query") // http://localhost:8080/students/query?id=1
+    public ResponseEntity<Student> getStudent(@RequestParam("id") Long id){
+
+        Student student = studentService.findStudent(id);
+        return ResponseEntity.ok(student); //  return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+    //!!! Get a Student by ID via PathVariable
+    @GetMapping("{id}")
+    public ResponseEntity<Student> getStudentWithPath(@PathVariable("id") Long id ){
+        Student student = studentService.findStudent(id);
+        return ResponseEntity.ok(student);
 
     }
+
+    //!!! Delete Student with id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, String>> deleteStudent(@PathVariable("id") Long id){
+        studentService.deleteStudent(id);
+
+        Map<String,String> map = new HashMap<>();
+        map.put("message", "Student is deleted successfuly");
+        map.put("status", "true");
+
+        return new ResponseEntity<>(map, HttpStatus.OK); // return ResponseEntity.ok(map);
+
+
+    }
+
+
 
 }
